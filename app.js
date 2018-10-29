c3462ce9e23bc1ebd6e39642281b2ff32b2ac009
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2018-10-29 19:51:54 +0800

    a bug fixed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes= require('./routes/admin');
 const customerRoutes= require('./routes/shope');
 const app = express();
 
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: false }));// its a must to parse body in middlewares for us ny using req.body 
 
 app.use('/admin',adminRoutes);
@@ -15,7 +16,7 @@ app.use(customerRoutes);
 
 
 app.use((req,res,next)=>{
-  res.status(404).sendFile(path.join(rootDir,'views','404.html' ))
+  res.status(404).sendFile(path.join(__dirname,'views','404.html' ))
 })
 
 app.listen(4000);
