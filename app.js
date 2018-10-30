43fbe6296e877f0616f15aeda7301c6f1ddace35
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2018-10-30 22:46:09 +0800

    ejs templating engin is added

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const express = require('express');
 const path = require('path');
 
+const express = require('express');
 const bodyParser = require('body-parser');
 
-const rootDir= require('./utility/path');
-const adminRoutes= require('./routes/admin');
-const customerRoutes= require('./routes/shope');
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')))
-app.use(bodyParser.urlencoded({ extended: false }));// its a must to parse body in middlewares for us ny using req.body 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use('/admin',adminRoutes);
-app.use(customerRoutes);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
-  res.status(404).sendFile(path.join(__dirname,'views','404.html' ))
-})
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
-app.listen(4000);
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
